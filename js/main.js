a4ba71656ce420d185c03433691dd70fe2e98dd9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-01-27 11:50:39 +0700

    myPorto | hamb-menu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.menu');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const iconBars = document.querySelector('.icon-bars');
+const iconClose = document.querySelector('.icon-close');
+
+function displayMenu() {
+    
+}
