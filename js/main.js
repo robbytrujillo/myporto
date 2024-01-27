d2ec531d8986b86555b2b42b12899fdd4a30d3a9
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-01-27 13:06:00 +0700

    myPorto | hamb-menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-const menu = document.querySelector('.menu');
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const iconBars = document.querySelector('.icon-bars');
-const iconClose = document.querySelector('.icon-close');
+const menu = document.querySelector(".menu");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const iconBars = document.querySelector(".icon-bars");
+const iconClose = document.querySelector(".icon-close");
 
 function displayMenu() {
-    
+  if (menu.classList.contains("tampil")) {
+    menu.classList.remove("tampil");
+    iconBars.computedStyleMap.display = "inline";
+    iconClose.computedStyleMap.display = "none";
+  } else {
+    menu.classList.add("tampil");
+    iconBars.computedStyleMap.display = "none";
+    iconClose.computedStyleMap.display = "inline";
+  }
 }
